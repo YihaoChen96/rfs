ad9265ed799539c9efd943ffc2ef4f0ee965ed6b
YihaoChen96 <yc2288@cornell.edu>
2020-06-05 15:02:51 -0400

    use transform lambda wrapper

--- dataset/transform_cfg.py
+++ dataset/transform_cfg.py
@@ -4,7 +4,6 @@ import numpy as np
 from PIL import Image
 import torchvision.transforms as transforms
 
-
 mean = [120.39586422 / 255.0, 115.59361427 / 255.0, 104.54012653 / 255.0]
 std = [70.68188272 / 255.0, 68.27635443 / 255.0, 72.54505529 / 255.0]
 normalize = transforms.Normalize(mean=mean, std=std)
@@ -12,17 +11,17 @@ normalize = transforms.Normalize(mean=mean, std=std)
 
 transform_A = [
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.RandomCrop(84, padding=8),
         transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
         transforms.RandomHorizontalFlip(),
-        lambda x: np.asarray(x),
+        transforms.Lambda(lambda x: np.asarray(x)),
         transforms.ToTensor(),
         normalize
     ]),
 
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.ToTensor(),
         normalize
     ])
@@ -30,16 +29,16 @@ transform_A = [
 
 transform_A_test = [
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.RandomCrop(84, padding=8),
         transforms.RandomHorizontalFlip(),
-        lambda x: np.asarray(x),
+        transforms.Lambda(lambda x: np.asarray(x)),
         transforms.ToTensor(),
         normalize
     ]),
 
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.ToTensor(),
         normalize
     ])
@@ -51,17 +50,17 @@ std = [0.2675, 0.2565, 0.2761]
 normalize_cifar100 = transforms.Normalize(mean=mean, std=std)
 transform_D = [
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.RandomCrop(32, padding=4),
         transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
         transforms.RandomHorizontalFlip(),
-        lambda x: np.asarray(x),
+        transforms.Lambda(lambda x: np.asarray(x)),
         transforms.ToTensor(),
         normalize_cifar100
     ]),
 
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.ToTensor(),
         normalize_cifar100
     ])
@@ -69,16 +68,16 @@ transform_D = [
 
 transform_D_test = [
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
-        lambda x: np.asarray(x),
+        transforms.Lambda(lambda x: np.asarray(x)),
         transforms.ToTensor(),
         normalize_cifar100
     ]),
 
     transforms.Compose([
-        lambda x: Image.fromarray(x),
+        transforms.Lambda(lambda x: Image.fromarray(x)),
         transforms.ToTensor(),
         normalize_cifar100
     ])
