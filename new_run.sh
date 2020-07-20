0c1be6989ec4b24a5d98a477d3df8704d2cb1211
YihaoChen96 <yc2288@cornell.edu>
2020-07-20 09:09:22 -0400

    add running script

--- new_run.sh
+++ new_run.sh
@@ -1,2 +1,4 @@
-python eval_fewshot.py --model_path path/to/save/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/resnet12_last.pth --data_root path/to/data_root --n_shots 580 --n_aug_support_samples 1
+python eval_fewshot.py --model_path path/to/save/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/resnet12_last.pth --data_root path/to/data_root --n_shots 10 --n_aug_support_samples 1 --n_ways 10
+
+# python eval_fewshot.py --model_path path/to/save/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/resnet12_last.pth --data_root path/to/data_root --n_shots 40 --n_aug_support_samples 1 --n_ways 2 --num_workers 0
 
