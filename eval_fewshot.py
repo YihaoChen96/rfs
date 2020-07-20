f0136f7f6726c178958ce1a7f7d427a41a2de9d2
YihaoChen96 <yc2288@cornell.edu>
2020-07-20 09:07:42 -0400

    add fine tuning

--- eval_fewshot.py
+++ eval_fewshot.py
@@ -160,23 +160,23 @@ def main():
     # print('val_acc_feat: {:.4f}, val_std: {:.4f}, time: {:.1f}'.format(val_acc_feat, val_std_feat, val_time))
 
     ### Use cosine similarity ###
-    # start = time.time()
-    # test_acc, test_std = meta_test(model, meta_testloader, classifier = "Cosine")
-    # test_time = time.time() - start
-    # print('test_acc: {:.4f}, test_std: {:.4f}, time: {:.1f}'.format(test_acc, test_std, test_time))
+    start = time.time()
+    test_acc, test_std = meta_test(model, meta_testloader, classifier = "Cosine")
+    test_time = time.time() - start
+    print('test_acc: {:.4f}, test_std: {:.4f}, time: {:.1f}'.format(test_acc, test_std, test_time))
 
-    # start = time.time()
-    # test_acc_feat, test_std_feat = meta_test(model, meta_testloader, use_logit=False, classifier = "Cosine")
-    # test_time = time.time() - start
-    # print('test_acc_feat: {:.4f}, test_std: {:.4f}, time: {:.1f}'.format(test_acc_feat, test_std_feat, test_time))
+    start = time.time()
+    test_acc_feat, test_std_feat = meta_test(model, meta_testloader, use_logit=False, classifier = "Cosine")
+    test_time = time.time() - start
+    print('test_acc_feat: {:.4f}, test_std: {:.4f}, time: {:.1f}'.format(test_acc_feat, test_std_feat, test_time))
 
     ### Fine Tuning ###
-    start = time.time()
-    for i in range(5):
-        print("Epoch %s" % i)
-        (test_acc, test_std), losses = meta_test_finetune(model, meta_testloader)
-        test_time = time.time() - start
-        print('test_acc: {:.4f}, test_std: {:.4f}, loss_mean: {:.4f}, time: {:.1f}'.format(test_acc, test_std, np.mean(losses), test_time))
+    # start = time.time()
+    # for i in range(5):
+    #     print("Epoch %s" % i)
+    #     (test_acc, test_std), losses = meta_test_finetune(model, meta_testloader)
+    #     test_time = time.time() - start
+    #     print('test_acc: {:.4f}, test_std: {:.4f}, loss_mean: {:.4f}, time: {:.1f}'.format(test_acc, test_std, np.mean(losses), test_time))
 
     # start = time.time()
     # test_acc_feat, test_std_feat = meta_test_finetune(model, meta_testloader, use_logit=False)
